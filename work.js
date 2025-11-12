ceeee29ac2cb59b7a914e84065f7436bcbdf981a
purwar2006 <abhishekpurwar2024@gmail.com>
2025-11-12 17:50:02 +0530

    DOM

--- work.js
+++ work.js
@@ -1,4 +1,4 @@
 
  document.getElementsByTagName('h2')[0].textContent="js"
   document.getElementsByTagName('div')[0].innerHTML="<h1>abhishek purwar</h1>"
-  
+  document.getElementsByTagName('p').style.Color="green"
